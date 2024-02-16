f8f8f65b29ee52d2c3c789d3286def9d67f4bf7f
Vikash Tiwary <88329254+uplVikash@users.noreply.github.com>
2024-02-17 05:16:01 +0530

    board car UI added

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -61,7 +61,7 @@ export const BoardList = ({
       <h2 className="text-3xl">
         {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
         <NewBoardButton orgId={orgId} />
         {data?.map((board) => (
           <BoardCard
@@ -76,7 +76,7 @@ export const BoardList = ({
             isFavorite={board.isFavorite}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
