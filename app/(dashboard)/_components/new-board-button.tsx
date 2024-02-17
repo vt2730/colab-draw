9514eb2a9b181c9f1c6023c1f5c79bd5fe996b90
Vikash Tiwary <88329254+uplVikash@users.noreply.github.com>
2024-02-17 05:43:55 +0530

    add new board button added

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import { toast } from "sonner";
+import { toast } from "sonner";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
-// import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 interface NewBoardButtonProps {
   orgId: string;
@@ -18,35 +18,34 @@ export const NewBoardButton = ({
   disabled,
 }: NewBoardButtonProps) => {
   const router = useRouter();
-//   const { mutate, pending } = useApiMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
-//   const onClick = () => {
-//     mutate({
-//       orgId,
-//       title: "Untitled"
-//     })
-//       .then((id) => {
-//         toast.success("Board created");
-//         router.push(`/board/${id}`);
-//       })
-//       .catch(() => toast.error("Failed to create board"));
-//   }
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: "Untitled"
+    })
+      .then((id) => {
+        toast.success("Board created");
+        router.push(`/board/${id}`);
+      })
+      .catch(() => toast.error("Failed to create board"));
+  }
 
   return (
-    // <button
-    //   disabled={pending || disabled}
-    //   onClick={onClick}
-    //   className={cn(
-    //     "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
-    //     (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
-    //   )}
-    // >
-    //   <div />
-    //   <Plus className="h-12 w-12 text-white stroke-1" />
-    //   <p className="text-sm text-white font-light">
-    //     New board
-    //   </p>
-    // </button>
-    <></>
+    <button
+      disabled={pending || disabled}
+      onClick={onClick}
+      className={cn(
+        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+        (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+      )}
+    >
+      <div />
+      <Plus className="h-12 w-12 text-white stroke-1" />
+      <p className="text-xs text-white font-light">
+        New board
+      </p>
+    </button>
   );
 };
